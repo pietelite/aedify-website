cbb91a9844ab8e3c8a2e6db5cde6af8f0abf6b18
SL <58554900+imagj@users.noreply.github.com>
2020-02-23 14:36:37 -0600

    test

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@
 !(function($) {
   "use strict";
 
-  // Toggle .header-scrolled class to #header when page is scrolled
+  // /Toggle .header-scrolled class to #header when page is scrolled
   $(window).scroll(function() {
     if ($(this).scrollTop() > 100) {
       $('#header').addClass('header-scrolled');
